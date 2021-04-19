267a64a7095c62870909a455f527ffc489ee338e
sundevil <zloasd@ya.ru>
2021-04-19 21:57:04 +0300

    first clear

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    todo-list
+  </div>
+</template>
+
+<script>
+export default {
+name: "ToDoList"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList></ToDoList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ToDoList from '@/components/ToDoList';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ToDoList
   }
 }
 </script>
