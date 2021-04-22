6b28e55b910dac13d6b2cae91b95e2f8d9263434
sundevil <zloasd@ya.ru>
2021-04-22 12:37:48 +0300

    add 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <div>Not found 404</div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "notFonud404"
+}
+</script>
+
+<style scoped>
+
+</style>
