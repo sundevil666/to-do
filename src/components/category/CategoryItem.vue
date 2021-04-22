7af5d3a6e14a34feee41365df5f5d350f0b8cbf1
sundevil <zloasd@ya.ru>
2021-04-22 12:43:21 +0300

    add category item in main page

---
+++ src/components/category/CategoryItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <li class="list-group-item category-item">
+    <div class="font-weight-bold text-info h3 d-flex justify-content-between">
+      <span
+          class="pointer category-item__title"
+          @click="editToto(categories.id)"
+      >{{ categories.name }}</span>
+      <span
+          class="category-item__remove pointer"
+          @click="$emit('click')"
+      >
+        &times;
+      </span>
+    </div>
+    <TodoPreview :categoriesId="categories.id" />
+  </li>
+</template>
+
+<script>
+import TodoPreview from '@/components/todo/TodoPreview';
+
+export default {
+  name: 'CategoryItem',
+  props: {
+    categories: {
+      type: Object,
+      default: null,
+    }
+  },
+  components: {
+    TodoPreview,
+  },
+  methods: {
+    editToto (id) {
+      this.$router.push('/edit/' + id)
+      console.log(id);
+    },
+  }
+};
+</script>
+
+<style lang="stylus">
+  .pointer {
+    cursor pointer
+  }
+  .category-item__title:first-letter {
+    text-transform uppercase
+  }
+</style>
